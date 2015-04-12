16a673dafeeff90a9933c3d2d4ac3d62060bdea1
Cynthia O'Donnell <mimibambino@gmail.com>
2015-04-12 10:34:25 +0200

    refactor api call to a function

--- etsy_api.py
+++ etsy_api.py
@@ -10,20 +10,29 @@ query_params = {
 
 url = "https://openapi.etsy.com/v2/listings/active"
 
-response = requests.get(url, params = query_params)
-data = response.json()
-results = data['results']
+def call_etsy(url, params, offset = 0):
+    response = requests.get(url, params = query_params)
+    data = response.json()
+    count = data['count']
+    results = data['results']
 
-items = []
-for result in results:
-    item = {}
-    item['listing_id'] = result['listing_id']
-    item['price'] = result['price']
-    item['currency'] = result['currency_code']
-    item['quanitity'] = result['quantity']
-    item['fiber'] = result['materials']
-    item['images'] = result['Images']
+    items = []
+    for result in results:
+        item = {}
+        item['listing_id'] = result['listing_id']
+        item['price'] = result['price']
+        item['currency'] = result['currency_code']
+        item['quanitity'] = result['quantity']
+        item['fiber'] = result['materials']
+        item['images'] = result['Images']
+        item['description'] = result['description']
 
-    items.append(item)
+        items.append(item)
 
-pprint.pprint(items)
+    pprint.pprint(items)
+    print len(items)
+    print count
+    print offset
+    return items, count, offset
+
+call_etsy(url, query_params)
